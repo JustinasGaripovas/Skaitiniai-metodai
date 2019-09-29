e0e77fa144899f488835bfc2a3814992cbe0c93f
JustinasGaripovas <40699104+JustinasGaripovas@users.noreply.github.com>
2019-09-30 02:51:00 +0300

    Add files via upload

---
+++ Lab1/Pvz1/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+namespace Pvz1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(669, 394);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 412);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(669, 251);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(773, 643);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Baigti";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(692, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 25);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Stygu metodas F(x)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(692, 383);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Tiesinė algebra";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(692, 643);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Valyti";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(692, 217);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(156, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Parametrinės funkcijos";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(692, 43);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(156, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Neutono metodas F(x)";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(692, 72);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(156, 37);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Mažėjančių intervalų metodas F(x)";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(692, 115);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(156, 23);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Stygu metodas G(x)";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(692, 144);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(156, 23);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "Neutono metodas G(x)";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(692, 173);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(156, 38);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "Mažėjančių intervalų metodas G(x)";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 678);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Skaitiniai metodai ir Algoritmai";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+
+        /// <summary>
+        /// Inicializacijos veiksmai
+        /// </summary>
+        private void Initialize()
+        {
+            // pridedam timerius
+            Timerlist.Clear();
+            Timerlist.Add(timer1);
+            Timerlist.Add(timer2);
+            Timerlist.Add(timer3);
+            Timerlist.Add(timer4);
+            Timerlist.Add(timer5);
+        }
+
+        /// <summary>
+        /// Paruošiamas langas vaizdavimui
+        /// </summary>
+        public void PreparareForm(float xmin, float xmax, float ymin, float ymax)
+        {
+
+            float x_grids = 10;
+            //double xmin = 0; double xmax = 2 * Math.PI;
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = (xmax - xmin) / x_grids;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Interval = (xmax - xmin) / x_grids;
+            chart1.ChartAreas[0].AxisX.MajorTickMark.Interval = (xmax - xmin) / x_grids;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Font = new Font("Courier New", 8, FontStyle.Bold);
+
+            float y_grids = 10;
+            //double ymin = -1; double ymax = 1;
+            chart1.ChartAreas[0].AxisY.MajorGrid.Interval = (ymax - ymin) / y_grids;
+            chart1.ChartAreas[0].AxisY.LabelStyle.Interval = (ymax - ymin) / y_grids;
+            chart1.ChartAreas[0].AxisY.MajorTickMark.Interval = (ymax - ymin) / y_grids;
+            chart1.ChartAreas[0].AxisY.LabelStyle.Font = new Font("Courier New", 8, FontStyle.Bold);
+
+            chart1.ChartAreas[0].AxisX.Minimum = xmin;
+            chart1.ChartAreas[0].AxisX.Maximum = xmax;
+            chart1.ChartAreas[0].AxisY.Minimum = ymin;
+            chart1.ChartAreas[0].AxisY.Maximum = ymax;
+
+            chart1.Legends[0].Font = new Font("Times New Roman", 12, FontStyle.Bold);
+            chart1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            chart1.ChartAreas[0].CursorX.Interval = 0.01;
+            chart1.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
+            chart1.ChartAreas[0].CursorY.Interval = 0.01;
+            
+        }
+        #endregion
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Button button3;
+        private Button button2;
+        private Button button4;
+        private Button button5;
+        private Timer timer3;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+    }
+
+
+
+}
+
---
+++ Lab1/Pvz1/Form1.cs
@@ -0,0 +1,421 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Windows.Forms.DataVisualization.Charting;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Factorization;
+
+namespace Pvz1
+{
+    public partial class Form1 : Form
+    {
+        List<Timer> Timerlist = new List<Timer>();
+
+        const double precisionPoint = 1e-6;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        // ---------------------------------------------- PUSIAUKIRTOS METODAS ----------------------------------------------
+
+        private double F(double x)
+        {
+            return (double)
+                (-1.29*Math.Pow(x, 4))+
+                (+5.08*Math.Pow(x, 3))+
+                (-2.76*Math.Pow(x, 2))+
+                (-6.31*Math.Pow(x, 1))+
+                + 4.10;
+        }
+
+        private double Fcustom(double m)
+        {
+            return (double)(50 * Math.Pow(Math.E, (-0.15 * 3) / m)) + 
+                (((9.8 * m) / 0.15) * 
+                (Math.Pow(Math.E, (-0.15 * 3) / m) - 1)) - 14;
+        }
+
+        private double FD(double x)
+        {
+            return (double)
+               (-5.16 * Math.Pow(x, 3)) +
+               (+15.24 * Math.Pow(x, 2)) +
+               (-5.52 * Math.Pow(x, 1)) +
+               -6.31;
+        }
+
+        private double G(double x)
+        {
+            return (double)(x * Math.Pow(Math.Cos(x), 2)) - (Math.Pow(x / 2, 2));
+        }
+
+        private double GD(double x)
+        {
+            return (double) Math.Pow(Math.Cos(x), 2) - (x * Math.Sin(x*2)) - x/2;
+        }
+
+        public delegate double CurrentFucntion(double x);
+        public delegate double CurrentFucntionD(double x);
+        Series FNC, XMID, CHORDS, NEWTON, ITTERATION;
+
+        private void SeriesInitializer()
+        {
+            CHORDS = chart1.Series.Add("CHORDS");
+            CHORDS.MarkerStyle = MarkerStyle.Circle;
+            CHORDS.MarkerSize = 8;
+            CHORDS.ChartType = SeriesChartType.Point;
+            CHORDS.ChartType = SeriesChartType.Line;
+
+            NEWTON = chart1.Series.Add("NEWTON");
+            NEWTON.MarkerStyle = MarkerStyle.Circle;
+            NEWTON.MarkerSize = 8;
+            NEWTON.ChartType = SeriesChartType.Point;
+            NEWTON.ChartType = SeriesChartType.Line;
+
+            ITTERATION = chart1.Series.Add("ITTERATION");
+            ITTERATION.MarkerStyle = MarkerStyle.Circle;
+            ITTERATION.MarkerSize = 8;
+            ITTERATION.ChartType = SeriesChartType.Point;
+            ITTERATION.ChartType = SeriesChartType.Line;
+
+            XMID = chart1.Series.Add("XMID");
+            XMID.MarkerStyle = MarkerStyle.Circle;
+            XMID.ChartType = SeriesChartType.Point;
+            XMID.ChartType = SeriesChartType.Line;
+            XMID.MarkerSize = 8;
+
+            FNC = chart1.Series.Add("funkcija");
+            FNC.ChartType = SeriesChartType.Line;
+            FNC.MarkerSize = 8;
+
+        }
+
+        float x1, x2, xstep;
+        private void FormInitializer(float x1, float x2, CurrentFucntion function, string functionName)
+        {
+            ClearForm(); // išvalomi programos duomenys
+            PreparareForm(-5, 5, -10, 10);
+            SeriesInitializer();
+
+            this.x1 = x1; // izoliacijos intervalo pradžia
+            this.x2 = x2; // izoliacijos intervalo galas
+
+            xstep = 0.1F;
+
+            richTextBox1.AppendText("Iteracija         x            F(x)        x1          x2          F(x1)         F(x2)       \n");
+
+            double x = -50;
+            for (int i = 0; i < 5000; i++)
+            {
+                FNC.Points.AddXY(x, function(x));
+                x = x + (2 * Math.PI) / 50;
+
+                //Console.WriteLine("x->{0}",x);
+                Console.WriteLine("F(x)->{0}", function(x));
+
+            }
+
+            FNC.BorderWidth = 2;
+        }
+
+        public void ChordMethod(double x1, double x2, CurrentFucntion function)
+        {
+            int iii = 0;
+            int N = 1000;
+
+            double k = Math.Abs(function(x1) / function(x2));
+            double xmid = (x1 + (k * x2)) / (1 + k);
+
+            while (Math.Abs(function(xmid)) > precisionPoint && iii <= N)
+            {
+                if (DifferentSign(function(x1), function(xmid), function))
+                {
+                    x2 = xmid;
+                }
+                else
+                {
+                    x1 = xmid;
+                }
+
+                k = Math.Abs(function(x1) / function(x2));
+                xmid = (x1 + k * x2) / (1 + k);
+
+                iii = iii + 1;
+            }
+
+            richTextBox1.AppendText(String.Format(" {0,6:d}   {1,12:f7}  {2,12:f7} {3,12:f7} {4,12:f7} {5,12:f7} {6,12:f7}\n",
+                                    iii, xmid, function(xmid), x1, x2, function(x1), function(x2)));
+
+            CHORDS.Points.AddXY(xmid, 0);
+        }
+
+        public void NewtonMethod(double x1, double x2, CurrentFucntion function, CurrentFucntionD functionD)
+        {
+            int iii = 0;
+            int N = 1000;
+
+            double answer = x1;
+            double step = function(answer) / functionD(x1);
+
+            while (Math.Abs(function(answer)) > precisionPoint && iii <= N)
+            {
+                step = step = function(answer) / functionD(x1);
+                answer -= step;
+
+                iii++;
+            }
+
+            richTextBox1.AppendText(String.Format(" {0,6:d}   {1,12:f7}  {2,12:f7}\n",
+                                   iii, answer, function(answer)));
+
+            NEWTON.Points.AddXY(answer, 0);
+        }
+
+        bool found = false;
+        int itterations = 0;
+        public void DecreasingIntervalMethod(double xstart, double xend, double step, CurrentFucntion function)
+        {
+            if (found)
+                return;
+
+            if ( Math.Abs(Math.Abs(function(xstart+step)) - (Math.Abs(function(xstart)))) > precisionPoint)
+            { 
+                for (double i = xstart; i < xend; i += step)
+                {
+                    itterations++;
+
+                    if (DifferentSign(i, i + step, function))
+                    {
+                        DecreasingIntervalMethod(i, i + step, step / 5, function);
+                    }
+                }
+            }
+            else
+            {
+                found = true;
+
+                if (found)
+                {
+                    richTextBox1.AppendText(String.Format(" {0,6:d}   {1,12:f7}  {2,12:f7} {3,12:f7} {4,12:f7} {5,12:f7} {6,12:f7}\n",
+                    itterations, xstart + (step / 2), function(xstart + (step / 2)), xstart, xend, function(xstart), function(xend)));
+
+                    ITTERATION.Points.AddXY(xstart, 0);
+                    ITTERATION.Points.AddXY(xstart + step, 0);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FormInitializer(-3.89F, 3.89F, F, "F(x)");
+
+            for (float i = x1; i < x2; i += xstep)
+            {
+                if (DifferentSign(i, i + xstep, F))
+                {
+                    ChordMethod(i, i + xstep, F);
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FormInitializer(-3.89F, 3.89F, F, "F(x)");
+
+            for (float i = x1; i < x2; i += xstep)
+            {
+                if (DifferentSign(i, i + xstep, F))
+                {
+                    NewtonMethod(i, i + xstep,F,FD);
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            FormInitializer(-3.89F, 3.89F, F, "F(x)");
+
+            for (float i = x1; i < x2; i += xstep)
+            {
+                if (DifferentSign(i, i + xstep, F))
+                {
+                    DecreasingIntervalMethod(i, i + xstep, 0.05, F);
+                    found = false; itterations = 0;
+                }
+            }
+        }
+
+
+        // ---------------------------------------------- TEKSTINE FUNKCIJOS ----------------------------------------------
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FormInitializer(-10F, 10F, Fcustom, "F(m)");
+            for (float i = x1; i < x2; i += xstep)
+            {
+                if (DifferentSign(i, i + xstep, Fcustom))
+                {
+                    ChordMethod(i, i + xstep, Fcustom);
+                }
+            }
+        }
+
+        // ---------------------------------------------- PARAMETRINĖS FUNKCIJOS ----------------------------------------------
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            FormInitializer(-3.89F, 3.89F, G, "G(x)");
+
+            for (float i = x1; i < x2; i += xstep)
+            {
+                if (DifferentSign(i, i + xstep, G))
+                {
+                    ChordMethod(i, i + xstep, G);
+                }
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            FormInitializer(-3.89F, 3.89F, G, "G(x)");
+
+            for (float i = x1; i < x2; i += xstep)
+            {
+                if (DifferentSign(i, i + xstep, G))
+                {
+                    NewtonMethod(i, i + xstep, G, GD);
+                }
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            FormInitializer(-3.89F, 3.89F, G, "G(x)");
+
+            for (float i = x1; i < x2; i += xstep)
+            {
+                if (DifferentSign(i, i + xstep, G))
+                {
+                    DecreasingIntervalMethod(i, i + xstep, 0.05, G);
+                    found = false; itterations = 0;
+                }
+            }
+        }
+
+        // ---------------------------------------------- TIESINĖ ALGEBRA ----------------------------------------------
+
+        /// <summary>
+        /// Tiesine algebra (naudojama MathNet)
+        /// </summary>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+
+            double[,] x = { { 1, 2, 3 }, { 3, 4, 5 }, { 6, 5, 8 } };
+            // iš masyvo sugeneruoja matricą, is matricos išskiria eilutę - suformuoja vektorių
+            Matrix<double> m = Matrix<double>.Build.DenseOfArray(x);
+            Vector<double> v = m.Row(1);
+            richTextBox1.AppendText("\nMatrica m:\n");
+            richTextBox1.AppendText(m.ToString());
+
+            richTextBox1.AppendText("\nVektorius v:\n");
+            richTextBox1.AppendText(v.ToString());
+
+            richTextBox1.AppendText("\ntranspose(m):\n");
+            richTextBox1.AppendText(m.Transpose().ToString());
+
+            Matrix<double> vm = v.ToRowMatrix();
+            richTextBox1.AppendText("\nvm = v' - toRowMatrix()\n");
+            richTextBox1.AppendText(vm.ToString());
+
+            Vector<double> v1 = m * v;
+            richTextBox1.AppendText("\nv1 = m * v\n");
+            richTextBox1.AppendText(v1.ToString());
+            richTextBox1.AppendText("\nmin(v1)\n");
+            richTextBox1.AppendText(v1.Min().ToString());
+
+            Matrix<double> m1 = m.Inverse();
+            richTextBox1.AppendText("\ninverse(m)\n");
+            richTextBox1.AppendText(m1.ToString());
+
+            richTextBox1.AppendText("\ndet(m)\n");
+            richTextBox1.AppendText(m.Determinant().ToString());
+
+            // you must add reference to assembly system.Numerics
+            Evd<double> eigenv = m.Evd();
+            richTextBox1.AppendText("\neigenvalues(m)\n");
+            richTextBox1.AppendText(eigenv.EigenValues.ToString());
+            
+            LU<double> LUanswer = m.LU();
+            richTextBox1.AppendText("\nMatricos M LU skaida\n");
+            richTextBox1.AppendText("\nMatrica L:\n");
+            richTextBox1.AppendText(LUanswer.L.ToString());
+            richTextBox1.AppendText("\nMatrica U:\n");
+            richTextBox1.AppendText(LUanswer.U.ToString());
+            
+            QR<double> QRanswer = m.QR();
+            richTextBox1.AppendText("\nMatricos M QR skaida\n");
+            richTextBox1.AppendText("\nMatrica Q:\n");
+            richTextBox1.AppendText(QRanswer.Q.ToString());
+            richTextBox1.AppendText("\nMatrica R:\n");
+            richTextBox1.AppendText(QRanswer.R.ToString());
+
+            Vector<double> v3 = m.Solve(v);
+            richTextBox1.AppendText("\nm*v3 = v sprendziama QR metodu\n");
+            richTextBox1.AppendText(v3.ToString());
+            richTextBox1.AppendText("Patikrinimas\n");
+            richTextBox1.AppendText((m * v3 - v).ToString());
+        }
+
+
+        // ---------------------------------------------- KITI METODAI ----------------------------------------------
+
+        /// <summary>
+        /// Nustotoma ar f(num1) ir f(num2) ženklai yra skirtingi
+        /// </summary>
+        private bool DifferentSign(double num1, double num2, CurrentFucntion function)
+        {
+            return Math.Sign((double)function(num1)) != Math.Sign((double)function(num2));
+        }
+
+        /// <summary>
+        /// Uždaroma programa
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        
+        /// <summary>
+        /// Išvalomas grafikas ir consolė
+        /// </summary>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+        }
+        
+
+        public void ClearForm()
+        {
+            richTextBox1.Clear(); // isvalomas richTextBox1
+            // sustabdomi timeriai jei tokiu yra
+            foreach (var timer in Timerlist)
+            {
+                timer.Stop();
+            }
+
+            // isvalomos visos nubreztos kreives
+            chart1.Series.Clear();
+        }
+    }
+}
